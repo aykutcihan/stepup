b0d656531dcb7d1e66da4b563ff33502112ba692
aykutcihan <aykutcihandemir@protonmail.com>
2026-05-07 07:26:58 +0200

    feat(fe-us-002): add login router to api.tsx

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,12 +1,16 @@
 import { Routes, Route } from 'react-router-dom'
 import RegisterPage from '@/pages/RegisterPage'
 import InviteUserPage from '@/pages/hr/InviteUserPage'
+import LoginPage from '@/pages/LoginPage'
+
 
 export default function App() {
   return (
     <Routes>
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/hr/invite-user" element={<InviteUserPage />} />
+      <Route path="/login" element={<LoginPage />} />
+
     </Routes>
   )
 }
