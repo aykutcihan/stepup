8486fc0677f2ff746481289914697acf6b8bf882
aykutcihan <aykutcihandemir@protonmail.com>
2026-05-10 15:20:31 +0200

    feat(fe-us-005): add update me to user service

--- apps/frontend/src/features/users/services/userService.ts
+++ apps/frontend/src/features/users/services/userService.ts
@@ -4,6 +4,12 @@ import { API } from '@/constants/apiEndpoints'
 
 type UserResponse = components['schemas']['UserResponse']
 type UserUpdate = components['schemas']['UserUpdate']
+type UserProfileUpdate = components['schemas']['UserProfileUpdate']
+
+export async function updateMe(data: UserProfileUpdate): Promise<UserResponse> {
+  const res = await apiClient.patch(API.USERS.UPDATE_ME, data)
+  return res.data
+}
 
 export async function getUsers(): Promise<UserResponse[]> {
   const res = await apiClient.get(API.USERS.LIST)
